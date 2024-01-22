43d3d968aadb811b3c531ee7014b48d7e3236288
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-01-22 13:46:31 +0530

    Responsive for all screen

--- script.js
+++ script.js
@@ -103,10 +103,7 @@ function cursor() {
             top: e.y,
         });
     });
-    Shery.makeMagnet("#nav h4,#nav svg:nth-child(1)", {
-        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-        duration: .5,
-    });
+
 
     const videoContainer = document.querySelector("#video-con");
     const video = document.querySelector("#video-con video");
@@ -153,17 +150,28 @@ function cursor() {
         }
         isVideoPlaying = !isVideoPlaying;
     });
-    Shery.makeMagnet(".circle-div,.element", {
-        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-        duration: 1,
-    });
+  
 };
 function sheryAnimation() {
-    Shery.imageEffect(".image-div", {
-        style: 5,
-        config: { "a": { "value": 0.69, "range": [0, 30] }, "b": { "value": -0.97, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749637004141 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.21, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2], "_gsap": { "id": 3 } }, "discard_threshold": { "value": 0.39, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.29, "range": [0, 2] }, "noise_scale": { "value": 22.9, "range": [0, 100] } },
-        gooey: true,
-    });
+
+    if (!/Android|webOS|iPhone|iPod|iPad|BlackBerry|Windows|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        Shery.imageEffect(".image-div", {
+            style: 5,
+            config: { "a": { "value": 0.69, "range": [0, 30] }, "b": { "value": -0.97, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749637004141 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.21, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2], "_gsap": { "id": 3 } }, "discard_threshold": { "value": 0.39, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.29, "range": [0, 2] }, "noise_scale": { "value": 22.9, "range": [0, 100] } },
+            gooey: true,
+        });
+        Shery.makeMagnet("#nav h4,#nav svg:nth-child(1)", {
+            ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+            duration: .5,
+        });
+        Shery.makeMagnet(".circle-div,.element", {
+            ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+            duration: 1,
+        });
+      }
+      
+   
+  
 };
 function flagAnimation() {
     document.addEventListener("mousemove", function (e) {
