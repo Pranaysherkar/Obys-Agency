1a659622b80a1044346daf4212f9e57f00193c43
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-01-24 15:46:45 +0530

    gsap-scroll animation added

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ function cursor() {
             });
         });
     });
-    
+
     video.addEventListener("mouseleave", function () {
         gsap.to(cursorElement, {
             opacity: 1,
@@ -131,9 +131,8 @@ function cursor() {
         gsap.to(btnElement, {
             top: "-10%",
             left: "80%",
-            scale:1,
         });
-        
+
     });
 
     videoContainer.addEventListener("click", function () {
@@ -150,28 +149,26 @@ function cursor() {
         }
         isVideoPlaying = !isVideoPlaying;
     });
-  
+
 };
 function sheryAnimation() {
 
-    if (!/Android|webOS|iPhone|iPod|iPad|BlackBerry|Windows|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        Shery.imageEffect(".image-div", {
-            style: 5,
-            config: { "a": { "value": 0.69, "range": [0, 30] }, "b": { "value": -0.97, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7272749637004141 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1.21, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.46, "range": [0, 10] }, "metaball": { "value": 0.2, "range": [0, 2], "_gsap": { "id": 3 } }, "discard_threshold": { "value": 0.39, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.29, "range": [0, 2] }, "noise_scale": { "value": 22.9, "range": [0, 100] } },
-            gooey: true,
-        });
-        Shery.makeMagnet("#nav h4,#nav svg:nth-child(1)", {
-            ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-            duration: .5,
-        });
-        Shery.makeMagnet(".circle-div,.element", {
-            ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-            duration: 1,
-        });
-      }
-      
-   
-  
+    Shery.imageEffect(".image-div", {
+        style: 2,
+        config: { "resolutionXY": { "value": 100 }, "distortion": { "value": true }, "mode": { "value": -10 }, "mousemove": { "value": 3 }, "modeA": { "value": 1 }, "modeN": { "value": 3 }, "speed": { "value": 1, "range": [-500, 500], "rangep": [-10, 10] }, "frequency": { "value": 50, "range": [-800, 800], "rangep": [-50, 50] }, "angle": { "value": 0.5, "range": [0, 3.141592653589793] }, "waveFactor": { "value": 1.4, "range": [-3, 3] }, "color": { "value": 10212607 }, "pixelStrength": { "value": 3, "range": [-20, 100], "rangep": [-20, 20] }, "quality": { "value": 5, "range": [0, 10] }, "contrast": { "value": 1, "range": [-25, 25] }, "brightness": { "value": 1, "range": [-1, 25] }, "colorExposer": { "value": 0.18, "range": [-5, 5] }, "strength": { "value": 0.2, "range": [-40, 40], "rangep": [-5, 5] }, "exposer": { "value": 8, "range": [-100, 100] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 0.7666557722625823 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": false }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 1.5, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": false }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 0 }, "noise_speed": { "value": 0.76, "range": [0, 10] }, "metaball": { "value": 0.6, "range": [0, 2] }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.37, "range": [0, 2] }, "noise_scale": { "value": 7.63, "range": [0, 100] }, "a": { "value": 1.37, "range": [0, 30] }, "b": { "value": -0.91, "range": [-1, 1] } },
+        gooey: true,
+    });
+    Shery.makeMagnet("#nav h4,#nav svg:nth-child(1)", {
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: .5,
+    });
+    Shery.makeMagnet(".circle-div,.element", {
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 1,
+    });
+
+
+
 };
 function flagAnimation() {
     document.addEventListener("mousemove", function (e) {
@@ -193,9 +190,324 @@ function flagAnimation() {
         });
     });
 };
+function scrollAnimation() {
+    let x = gsap.timeline();
+
+    x.from("#page3 h1", {
+        opacity: 0,
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 h1",
+            scroller: "#main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 3,
+        },
+    });
+    x.to("#page3 #line1", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#page3 #line1",
+            scroller: "#main",
+            // markers: true,
+            start: "top 75%",
+            end: "top 70%",
+            scrub: 3,
+        }
+    });
+    x.from("#page3 #element1 h2", {
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 #element1 h2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    x.from("#page3 #element1 .info", {
+        y: 100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#element1 .info",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    x.to("#page3 #element1 .under-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#element1 .under-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 100%",
+            end: "top 90%",
+            scrub: 3
+        }
+    });
+    //-----
+    x.from("#page3 #element--1 h2", {
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 #element--1 h2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    x.from("#page3 #element--1 .info", {
+        y: 100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#element--1 .info",
+            scroller: "#main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 3
+        }
+    });
+    x.to("#page3 #element--1 .under-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#element--1 .under-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+
+    //-----
+    x.from("#page3 #element2 h2", {
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 #element2 h2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 3
+        }
+    });
+    x.from("#page3 #element2 .info", {
+        y: 100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#element2 .info",
+            scroller: "#main",
+            // markers: true,
+            start: "top 100%",
+            end: "top 90%",
+            scrub: 3
+        }
+    });
+    x.to("#page3 #element2 .under-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#element2 .under-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    //-----
+    x.from("#page3 #element3 h2", {
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 #element3 h2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 60%",
+            end: "top 50%",
+            scrub: 3
+        }
+    });
+    x.from("#page3 #element3 .info", {
+        y: 100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#element3 .info",
+            scroller: "#main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 3
+        }
+    });
+    x.to("#page3 #element3 .under-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#element3 .under-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    //-----
+    x.from("#page3 #element4 h2", {
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 #element4 h2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 60%",
+            scrub: 3
+        }
+    });
+    x.from("#page3 #element4 .info", {
+        y: 100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#element4 .info",
+            scroller: "#main",
+            // markers: true,
+            start: "top 100%",
+            end: "top 90%",
+            scrub: 3
+        }
+    });
+
+    x.to("#page3 #element4 .under-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#element4 .under-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    //-----
+    x.from("#page3 #element5 h2", {
+        y: 100,
+        scrollTrigger: {
+            trigger: "#page3 #element5 h2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 90%",
+            end: "top 80%",
+            scrub: 3
+        }
+    });
+    x.from("#page3 #element5 .info", {
+        y: 100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#element5 .info",
+            scroller: "#main",
+            // markers: true,
+            start: "top 80%",
+            end: "top 70%",
+            scrub: 3
+        }
+    });
+    x.to("#page3 #element5 .under-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#element4 .under-line",
+            scroller: "#main",
+            start: "top 70%",
+            end: "top 60%",
+            scrub: 3,
+        }
+    });
+    x.from(" #container h1", {
+        opacity: 0,
+        y: 100,
+        scrollTrigger: {
+            trigger: "#container h1",
+            scroller: "#main",
+            // markers: true,
+            start: "top 70%",
+            end: "top 60%",
+            scrub: 3,
+        },
+    });
+    x.to("#container .bottom-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#container .bottom-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 75%",
+            end: "top 70%",
+            scrub: 3,
+        }
+    });
+    x.from("#container p", {
+        opacity:0,
+        y:100,
+        scrollTrigger: {
+            trigger: "#container p",
+            scroller: "#main",
+            // markers: true,
+            start: "top 70%",
+            end: "top 55%",
+            scrub: 4,
+        },
+    });
+    x.to("#container #bottom-line2", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#container #bottom-line2",
+            scroller: "#main",
+            // markers: true,
+            start: "top 75%",
+            end: "top 70%",
+            scrub: 3,
+        }
+    });
+    x.from(" #footer h1", {
+        opacity: 0,
+        y: 100,
+        scrollTrigger: {
+            trigger: "#footer h1",
+            scroller: "#main",
+            // markers: true,
+            start: "top 70%",
+            end: "top 60%",
+            scrub: 3,
+        },
+    });
+    x.to("#footer .bottom-line", {
+        "--width": "100%",
+        scrollTrigger: {
+            trigger: "#footer .bottom-line",
+            scroller: "#main",
+            // markers: true,
+            start: "top 65%",
+            end: "top 55%",
+            scrub: 3,
+        }
+    });
+
+}
 
 loadingAnimation();
 locomotive();
-cursor();
-sheryAnimation();
-flagAnimation();
+
+if (!/Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    cursor();
+    flagAnimation();
+    scrollAnimation();
+    sheryAnimation();
+}
+else {
+    console.log("prany sheck code");
+}
+
